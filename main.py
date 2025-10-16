c57109441a3f5c735554bd15b0a6319df1bbfb71
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-10-15 21:47:44 -0400

    CH1 Lessons 1 thru 6 completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -9,24 +12,40 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true')
+    #args = parser.parse_args()
+    args, _ = parser.parse_known_args()
 
-usage = response.usage_metadata
+    if len(sys.argv) < 2:
+        print("Usage: uv run main.py <ai prompt>")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
 
-prompt_tokens = usage.prompt_token_count
-response_tokens = usage.candidates_token_count
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+    )
+    print(response.text)
+
+    usage = response.usage_metadata
+
+    prompt_tokens = usage.prompt_token_count
+    response_tokens = usage.candidates_token_count
+
+    if args.verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
-#def main():
-    #print("Hello from aiagent!")
 
 
-#if __name__ == "__main__":
-    #main()
+
+if __name__ == "__main__":
+    main()
